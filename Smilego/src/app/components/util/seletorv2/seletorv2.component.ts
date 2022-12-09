fe26c11a2bead06a209f18f36cf4c71e520ab3c5
Adalberto Gonçalves da Silva <adalberto.goncalves.silva@gmail.com>
2022-12-09 20:08:01 -0300

    Atualizando a lista

--- Smilego/src/app/components/util/seletorv2/seletorv2.component.ts
+++ Smilego/src/app/components/util/seletorv2/seletorv2.component.ts
@@ -5,6 +5,8 @@ interface mensagem {
   texto: String
 }
 
+let lista:[]=[];
+
 @Component({
   selector: 'app-seletorv2',
   templateUrl: './seletorv2.component.html',
@@ -13,23 +15,22 @@ interface mensagem {
 
 export class Seletorv2Component implements OnInit {
 
-lista:mensagem[] = [{id:'13', texto:'Décimo terciro teste'},
-          {id: '4', texto:'Quarto teste'},
-          {id: '1', texto:'Primiro teste'},
-          {id: '8', texto:'Oitavo teste'},
-          {id: '35', texto:'Trigésimo quinto teste'},
-          {id: '4', texto:'Quarto teste pela segunda vez'},
-          {id: '2', texto:'Segundo teste'}
-                      ];
+lista = [{id:'13', texto:'Décimo terciro teste'},
+         {id: '4', texto:'Quarto teste'},
+         {id: '1', texto:'Primiro teste'},
+         {id: '8', texto:'Oitavo teste'},
+         {id: '35', texto:'Trigésimo quinto teste'},
+         {id: '4', texto:'Quarto teste pela segunda vez'},
+         {id: '2', texto:'Segundo teste'}];
 
   constructor() {}
 
   ngOnInit(): void {
-   // this.converter();
-    this.lista = this.lista.map(function(item:{id:any; string:any; texto:string}){
+
+     this.lista = this.lista.map(function(item){
         return {
                id: isNaN(Number(item.id)) ? item.id:Number(item.id),
-              texto: item.texto
+               texto: item.texto
               }
     });
   }
